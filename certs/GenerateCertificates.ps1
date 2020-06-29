aa4db88cfeb883554e3d98725ed9891f84002820
MissakaI <m.iddamalgoda@gmail.com>
2020-06-30 00:14:29 +0530

    Added initial packages and generate RSA certificates.

---
+++ certs/GenerateCertificates.ps1
@@ -0,0 +1,7 @@
+﻿$pwd = ConvertTo-SecureString -String "euphoria" -AsPlainText
+
+$cert = New-SelfSignedCertificate -Type Custom -KeyUsageProperty Sign -KeyUsage DigitalSignature -KeyAlgorithm RSA -KeyLength 2048 -Subject "Euphoria DigitalSignature" -KeyFriendlyName "Euphoria" -KeyDescription "Digital Signature to sign tokens for Euphoria" -CertStoreLocation "Cert:\CurrentUser\My"
+
+Export-Certificate -Cert $cert -FilePath .\euphoria-public.cer
+
+Export-PfxCertificate -Cert $cert -FilePath .\euphoria-private.pfx -Password $pwd
---
+++ certs/README.md
@@ -0,0 +1,9 @@
+# Generate Certificates
+Use the following scripts to generate your own set of certificates
+
+## MS PowerShell
+To generate your self-signed certificates
+
+    Unblock-File -Path .\GenerateCertificates.ps1
+    .\GenerateCertificates.ps1
+
